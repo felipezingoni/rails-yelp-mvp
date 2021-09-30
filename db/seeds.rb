6d934304ec4daeee19c4243e27bf764ad49035ca
Felipe Zingoni <felipezingoni.fz@gmail.com>
2021-09-29 23:11:39 -0300

    ejercicio terminado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Negroni', address: 'Recoleta', phone_number: '15666662', category: 'french')
+Restaurant.create(name: 'Don Pepe', address: 'Cordoba', phone_number: '15634662', category: 'japanese')
+Restaurant.create(name: 'Duko', address: 'Santa Fe', phone_number: '1563422262', category: 'italian')
